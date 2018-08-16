5e3602ef9cd437342e3513a425322054f83e78c4
Thanh Nguyen <nguyenphuongthanhf@gmail.com>
2018-08-16 17:14:13 +0700

    update

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import {connect} from "react-redux";
+import { bindActionCreators } from 'redux';
+import { selectBook } from '../actions/index';
 
 class BookList extends Component {
     renderList(){
+        console.log('Rerender Bookdetail');
         return this.props.books.map((book) => {
             return (
-                <li key={book.title} className="list-group-item">{book.title}</li>
+                <li 
+                    key={book.title} 
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
+                    {book.title}
+                </li>
             );
         });
     }
@@ -24,8 +32,11 @@ function mapStateToProps(state){
     };
 }
 
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({selectBook: selectBook }, dispatch);
+}
  
-export default  connect( mapStateToProps )( BookList );
+export default  connect(mapStateToProps, mapDispatchToProps )( BookList );
 
 
 
